89298fe61903330fae7a08a5ec59f79fb2bc8bd7
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-05-26 21:10:04 -0500

    create player works

--- src/components/TeamContainer/TeamContainer.js
+++ src/components/TeamContainer/TeamContainer.js
@@ -3,10 +3,12 @@ import './TeamContainer.scss';
 import playerData from '../../helpers/data/playersData';
 import authData from '../../helpers/data/authData';
 import Player from '../Player/Player';
+import PlayerForm from '../PlayerForm/PlayerForm';
 
 class TeamContainer extends React.Component {
   state = {
     players: [],
+    formOpen: false,
   }
 
   getPlayers = () => {
@@ -23,15 +25,27 @@ class TeamContainer extends React.Component {
     playerData.deletePlayer(playerId)
       .then(() => this.getPlayers())
       .catch((err) => console.error('unable to delete player', err));
-  };
+  }
+
+  saveNewPlayer = (newPlayer) => {
+    playerData.savePlayer(newPlayer)
+      .then(() => {
+        this.getPlayers();
+        this.setState({ formOpen: false });
+      })
+      .catch((err) => console.error('unable to save player: ', err));
+  }
 
 
   render() {
-    const { players } = this.state;
+    const { players, formOpen } = this.state;
     const makePlayers = players.map((player) => <Player key={player.id} player={player} removePlayer={this.removePlayer}/>);
+
     return (
       <div className="TeamContainer">
         <h1 className="teamName text-white mt-3 mb-3">VANCOUVER CANUCKS</h1>
+        <button className="btn btn-success mb-3" onClick={() => this.setState({ formOpen: true })}>ADD NEW PLAYER</button>
+        { formOpen ? <PlayerForm saveNewPlayer={this.saveNewPlayer}/> : ''}
         <div className="d-flex flex-wrap">
           {makePlayers}
         </div>
